ef9e266f6703c7d1aad2abdead9002a1b43c0255
Filipa Barros <barrosfilipa.f@gmail.com>
2023-06-09 14:46:18 +0200

    done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  let game;
 
   startButton.addEventListener("click", function () {
     startGame();
@@ -8,5 +9,25 @@ window.onload = function () {
 
   function startGame() {
     console.log("start game");
+    game = new Game();
+
+    game.start();
   }
+
+  function keydownHandler(e) {
+    if (e.key === "ArrowUp") {
+      return (game.player.directionY = -1);
+    } else if (e.key === "ArrowLeft") {
+      return (game.player.directionX = -1);
+    } else if (e.key === "ArrowDown") {
+      return (game.player.directionY = 1);
+    } else if (e.key === "ArrowRight") {
+      return (game.player.directionX = 1);
+    }
+    e.preventdefault();
+
+    // game.player.move();
+  }
+
+  window.addEventListener("keydown", keydownHandler);
 };
